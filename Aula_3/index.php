779223fc17f3b84a93c4950ded7f30b25086e480
João Pedro <jplhfernandez@gmail.com>
2024-09-27 11:38:35 -0300

    aula

--- Aula_3/index.php
+++ Aula_3/index.php
@@ -2,18 +2,34 @@
 <body>    
     <?php
         include("conexao.php");
+        
+        $cnome = "";
+        $cusuario  = "";
+        $csaldo = "";
+        $csenha = "";
+        
+        if(!empty($_POST['selecionarcliente'])=="Atualizar")
+        {
+            $sql = "SELECT * FROM contas WHERE id=".$_POST['id'];
+            $dado = $conn -> query($sql);
+            if (!empty($dado)) 
+            {
+                while ($cliente = $dado -> fetch_assoc()) 
+                {
+                    $cnome = $cliente["nome"];
+                    $cusuario  = $cliente["usuario"];
+                    $csaldo = $cliente["saldo"];
+                    $csenha = $cliente["senha"];
+                }
+            }
+        }
+        
         if(!empty($_POST['id']))
         {
             $sql = "DELETE FROM contas WHERE id = ".$_POST['id'];
             $conn -> query($sql);
         }
 
-        if(!empty($_POST['id']))
-        {
-            $sql = "UPDATE FROM contas WHERE id = ".$_POST['id'];
-            $conn -> query($sql);
-        }
-    
         if (!empty($_POST['nome']))
         {
             $nome = $_POST["nome"];
@@ -36,20 +52,23 @@
             "</td> <td>".$row["saldo"]."</td> <td>
             <form action = 'index.php' method = 'POST' >
                 <input type = 'hidden' name = 'id' value = ' ".$row["id"]."' >
-                <input type = 'submit' value = 'Deletar' >
+                <input type = 'submit' name= 'selecionarcliente' value = 'Atualizar' >
+                <input type = 'submit' name= 'deletarcliente' value = 'Deletar' >
             </form>
             </tr>";
         }
     ?>
  
 <form action="index.php" method="POST">
-    Nome: <input type="text" name="nome"><br>
-    Usuario: <input type="text" name="usuario"><br>
-    Senha: <input type="text" name="senha"><br>
-    Saldo: <input type="text" name="saldo"><br>
+    Nome: <input type="text" name="nome" value ="<?php echo $cnome;?>"><br>
+    Usuario: <input type="text" name="usuario" value ="<?php echo $cusuario;?>"><br>
+    Senha: <input type="text" name="senha" value ="<?php echo $csenha;?>"><br>
+    Saldo: <input type="text" name="saldo" value ="<?php echo $csaldo;?>"><br>
+    <input type = 'hidden' name = 'id' value = '".$id."' >
     <input type="submit" value="Inserir">
+    <input type ="submit" value = 'Atualizar' >
 </form>
- 
+
 </body>
 </html>
  
---
+++ Locadora/conexao.php
@@ -0,0 +1,19 @@
+<?php
+    $servername = "localhost"; //Nome do Servidor.
+    $username = "root"; //Nome do Usuário.
+    $password = ""; //Senha do Usuário.
+    $dbname = "locadora"; //Nome do Banco de Dados.
+
+    //Criar Conexão
+    $conn = new mysqli($servername, $username, $password, $dbname);
+        // $conn é um objeto de conexão.
+
+    //Checar Conexão
+    if ($conn->connect_error)
+    {
+        die("Connection failed: ".$conn -> connect_error);
+    }
+    echo "Conectado com Sucesso.";
+        //As mensagens não são necessárias para o usuário final. Neste caso, elas estão sendo usuadas para facilitar no processo de programação.
+    echo "<br>";
+    ?>
