2f4d03be20f0cbd51b1ffc325e45cb9ff4738640
João Pedro <jplhfernandez@gmail.com>
2024-10-03 09:35:08 -0300

    Iniciando a atividade da segunda

--- Locadora/index.php
+++ Locadora/index.php
@@ -11,24 +11,26 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#">Locadora</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="inserir.php">Inserir</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="atualizar.php">Atualizar</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link2</a>
+                <a class="nav-link" href="deletar.php">Deletar</a>
               </li>
             </ul>
           </div>
         </div>
 </nav>
+<div class="caixa">
+</div>
 </body>
 </html>
