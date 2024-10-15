82edef7df9d8a90c2f7766ef9e0bcb520d122837
João Pedro <jplhfernandez@gmail.com>
2024-10-15 08:08:23 -0300

    chuleta para atividade

---
+++ chuleta Atividade MVC e POO/cliente/index.php
@@ -0,0 +1,9 @@
+<?php 
+   include "../admin/acesso_com.php"
+?>
+<h2>
+<strong> <?php echo $_GET['cliente']; ?></strong>, Bem vindo à sua área de cliente! 
+</h2>
+<a href="../admin/logout.php">
+    <span class="glyphicon glyphicon-log-out">sair</span>
+</a>
