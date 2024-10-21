a3d021f5441dedc2379933c74d03baa3d4004009
João Pedro <jplhfernandez@gmail.com>
2024-10-21 11:52:44 -0300

    pessoal, estão liberados

--- chuleta POO/admin/produtos_excluir.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include 'acesso_com.php';
-include '../conn/connect.php';
-
-$id = $_GET['id'];
-$excluirProduto = "DELETE FROM produtos WHERE id=$id";
-$resultado = $conn->query($excluirProduto);
-header ("location: produtos_lista.php");
-?>
--- chuleta POO/admin/tipos_excluir.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include 'acesso_com.php';
-include '../conn/connect.php';
-
-$id = $_GET['id'];
-$excluirTipo = "DELETE FROM tipos WHERE id=$id";
-$resultado = $conn->query($excluirTipo);
-header ("location: tipos_lista.php");
-?>
