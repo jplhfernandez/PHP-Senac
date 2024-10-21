e6196f36aaa38fdee5161e13e3bdbd8582d23d28
João Pedro <jplhfernandez@gmail.com>
2024-10-21 09:35:49 -0300

    calendarios

---
+++ calendar/index.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+include_once("conexao.php");
+$result_events = "SELECT id, title, color, start, end FROM events";
+$resultado_events = mysqli_query($conn, $result_events);
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset='utf-8' />
+		<title>Agenda</title>
+		<link href='css/fullcalendar.min.css' rel='stylesheet' />
+		<link href='css/fullcalendar.print.min.css' rel='stylesheet' media='print' />
+		<link href='css/personalizado.css' rel='stylesheet' />
+		<script src='js/jquery.min.js'></script>
+		<script src='js/moment.min.js'></script>
+		<script src='js/fullcalendar.min.js'></script>
+		<script src='locale/pt-br.js'></script>
+		<script>
+			$(document).ready(function() {
+				$('#calendar').fullCalendar({
+					header: {
+						left: 'prev,next today',
+						center: 'title',
+						right: 'month,agendaWeek,agendaDay'
+					},
+					defaultDate: Date(),
+					navLinks: true, // can click day/week names to navigate views
+					editable: true,
+					eventLimit: true, // allow "more" link when too many events
+					eventClick: function(event) {
+						$('#visualizar #id').text(event.id);
+						$('#visualizar #title').text(event.title);
+						$('#visualizar #start').text(event.start.format('DD/MM/YYYY HH:mm:ss'));
+						$('#visualizar #end').text(event.end.format('DD/MM/YYYY HH:mm:ss'));
+						$('#visualizar').modal('show');
+						return false;
+					},
+					
+					selectable: true,
+					selectHelper: true,
+					select: function(start, end){
+						$('#cadastrar #start').val(moment(start).format('DD/MM/YYYY HH:mm:ss'));
+						$('#cadastrar #end').val(moment(end).format('DD/MM/YYYY HH:mm:ss'));
+						$('#cadastrar').modal('show');						
+					},
+					events: [
+						<?php
+							while($row_events = mysqli_fetch_array($resultado_events)){
+								?>
+								{
+								id: '<?php echo $row_events['id']; ?>',
+								title: '<?php echo $row_events['title']; ?>',
+								start: '<?php echo $row_events['start']; ?>',
+								end: '<?php echo $row_events['end']; ?>',
+								color: '<?php echo $row_events['color']; ?>',
+								},<?php
+							}
+						?>
+					]
+				});
+			});
+			
+			//Mascara para o campo data e hora
+			function DataHora(evento, objeto){
+				var keypress=(window.event)?event.keyCode:evento.which;
+				campo = eval (objeto);
+				if (campo.value == '00/00/0000 00:00:00'){
+					campo.value=""
+				}
+			}
+		</script>
+	</head>
+	<body>
+		<div class="container">	
+			<div id='calendar'></div>
+		</div>
+	</body>
+</html>
